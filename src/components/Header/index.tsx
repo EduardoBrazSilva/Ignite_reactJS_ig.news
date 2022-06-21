1de399b2c5be37d084e63b20278dd3ddd819d445
Eduardo Braz <eduardobrazsilva45@gmail.com>
2022-06-21 16:33:27 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import { SingInButton } from '../SingInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+
+                <nav>
+                <a className={styles.active}>Home</a>
+                <a >Posts</a>
+                </nav>
+
+                <SingInButton />
+            </div>
+        </header>
+    );
+}
---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,5 @@
+export function SubscribeButton() {
+    return (
+        <h1></h1>
+    )
+}
